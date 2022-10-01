06c312382c538aeffbfb344e4e369f50c5013d29
Joel Wang <workworkjoel@gmail.com>
2022-10-01 14:44:06 +0300

    3.8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+
+morgan.token("body", function (req, res) {
+    if (req.method === "POST") {
+        return JSON.stringify(req.body)
+    }
+  return null;
+});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
   {
